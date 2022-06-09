7ba7eff15c0aee14ab483f3b3b76398659fe6d09
AalaapHegde <98975414+AalaapHegde@users.noreply.github.com>
2022-06-08 17:44:25 -0700

    Update wsgi.py

--- scrapsaver/wsgi.py
+++ scrapsaver/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scrap-saver.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scrapsaver.settings')
 
 application = get_wsgi_application()
